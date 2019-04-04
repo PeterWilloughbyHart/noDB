8663aa5b20eab12733c75cd30590de5f5533bde4
Peter Hart <Oliverdog4@gmail.com>
2019-04-04 10:06:30 -0500

    Commit 10:06AM

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+PORT = 4040;
+
+
+
+app.listen(PORT, console.log('Mic check one two'))
--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+  super(props) 
+
+    this.state = {
+      tab: 'Home'
+    }
+  }
+
+
+
+
+
+
+
   render() {
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+
+      <header className="main_header">
+        <header className="inner_header">
+          <header className="logo_header"><h1>SEA<span>IT</span><span id="Us"> A Tour of Our Oceans</span></h1></header>
         </header>
+      </header>
+
+      <main>
+        <div className="button_box">
+          <button>Enter</button>
+          <button>About Us</button>
+        </div>
+
+        <div className="submarine_space">
+          <div className="submarine">
+            <div className="sub_window"></div>
+          </div>
+        </div>
+      </main>
+
+
       </div>
     );
   }
 }
+
+
+
+
+
+
 
 export default App;
